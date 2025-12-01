d0dce41d4b5a4a028984240351f897ed7a6a7149
tseduro <ufuoma.aomreore@gmail.com>
2025-11-30 20:21:26 -0500

    authentication implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import {useState, useEffect} from 'react';
+import { createClient } from '@supabase/supabase-js';
+
+const supabase = createClient(
+  "https://elvnswrwvepdjcjspwil.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsdm5zd3J3dmVwZGpjanNwd2lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMjIzMzQsImV4cCI6MjA3OTU5ODMzNH0.xjxsxydWLjIlZqVkP7dPyHZfwXCPExnypg22VTJjQdM"
+)
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+  const[userData, setUserData] = useState(null);
+
+  useEffect(() => {
+
+    async function checkData() {
+
+      const{data, error} = await supabase.auth.getSession();
+      setUserData(data);
+    }
+
+    checkData();
+  }, [] )
+
+  async function signUp() {
+    
+    const {data, error} = await supabase.auth.signUp({
+      email:email,
+      password:password,
+    });
+
+    if(error){
+      alert("Error Signing Up");
+    }else{
+      setUserData(data);
+      console.log(data);
+    }
+  }
+
+  async function signIn() {
+    
+    const {data, error} = await supabase.auth.signInWithPassword({
+      email:email,
+      password:password,
+    });
+
+    if(error){
+      alert("Error Signing In");
+    }else{
+      setUserData(data);
+      console.log(data);
+    }
+  }
+
+  async function signOut() {
+    
+    const {data, error} = await supabase.auth.signOut();
+    setUserData(null);
+  }
+
+  return userData?.session != null ?(
+  <>
+  <div>
+    <h1>Hello, {userData.session.user.email}</h1>
+    <button onClick={signOut}>Sign Out</button>
+  </div>
+  </>
+  ):(
+  <>
+  <div>
+    <input 
+    onChange={(e) => {
+      setEmail(e.target.value)
+    }} 
+    type ="email" 
+    placeholder='Enter your email'
+    />
+    <input 
+    onChange={(e)=>{
+      setPassword(e.target.value)
+      }} 
+      type="password" 
+      placeholder='Choose a password' 
+    />
+    <button onClick={signUp}>Sign up</button>
+    <hr />
+    <input 
+    onChange={(e) => {
+      setEmail(e.target.value)
+      }} 
+      type ="email" 
+      placeholder='Enter your email'
+    />
+    <input 
+    onChange={(e) => 
+      {setPassword(e.target.value)
+    }} 
+    type="password" 
+    placeholder='Enter your password' 
+    />
+    <button onClick={signIn}>Login</button>  
+  </div>
+  </>
+)}
 
 export default App
