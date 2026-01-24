719cfd1e7591e47950e8e203c53944c532c00487
tseduro <ufuoma.aomreore@gmail.com>
2026-01-23 21:14:38 -0500

    feat: Finalized first css pass; Updated packages and modules for security

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,10 +28,12 @@ function Home() {
     <>
       <div>
         <Navbar />
-        <div>
-          <h2>Welcome!</h2>
+        <div className='text-center space-y-8'>
+          <h2 className="font-serif text-6xl font-bold text-balance gradient-text">Welcome Back!</h2>
+          <p className="text-xl text-muted-foreground leading-relaxed max-w-2xl mx-auto">Here's your progress report</p>
+
           <div>
-            <button onClick={fetchHabits} className='border border-border hover:bg-blue-600 px-2 rounded-lg'> test </button>
+            <button onClick={fetchHabits} className='border border-border hover:bg-blue-600 px-2 rounded-lg'> fetch habits </button>
           </div>
         </div>
       </div>
